0e6ba75f6d96aebd45bd9502c7aefe851ab1d24b
Glen Horton <glen.horton@gmail.com>
2016-05-12 16:30:48 -0400

    Update ingest-file.sh

--- ingest-file.sh
+++ ingest-file.sh
@@ -19,13 +19,12 @@ VERSIONABLE="true"
 DS_STATE="A"
 FLASH="true"
 
-echo "Deleteing the existing content datatream..."
+printf "\n\nDeleteing the existing content datatream...\n"
 $CURL_PATH -i -XDELETE "$FEDORA_URL/objects/$FILE_PID/datastreams/content" -u $FEDORA_USER:$FEDORA_PASSWORD
 
-echo "Creating new content datastream and upload file..."
+printf "\n\nCreating new content datastream and uploading file...\n"
 $CURL_PATH -i -H -XPOST -F file="@$FILE_NAME" -u $FEDORA_USER:$FEDORA_PASSWORD "$FEDORA_URL/objects/$FILE_PID/datastreams/content?dsLabel=$FILE_NAME&formatURI=$FORMAT_URI&checksumType=$CHECKSUM_TYPE&altIDs=$ALT_IDS&mimeType=$MIME_TYPE&controlGroup=$CONTROL_GROUP&versionable=$VERSIONABLE&dsState=$DS_STATE&flash=$FLASH"
 
-echo "Next steps in rails console:"
-echo "ActiveFedora::Base.reindex_everything"
-echo "Sufia.queue.push(CharacterizeJob.new('$FILE_PID'))"
-
+printf "\n\nNext steps in rails console:"
+printf "\nActiveFedora::Base.reindex_everything"
+printf "\nSufia.queue.push(CharacterizeJob.new('$FILE_PID'))\n"
